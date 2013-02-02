f0c2ade12e661ca804ed0914d4f45fa064bf83db
Sam Atkins <github@samatkins.co.uk>
2013-02-02 11:40:00 +0000

    Initial commit

---
+++ disco-dungeon-html/src/uk/co/samatkins/dungeon/client/GwtLauncher.java
@@ -0,0 +1,19 @@
+package uk.co.samatkins.dungeon.client;
+
+import uk.co.samatkins.dungeon.Game;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+
+public class GwtLauncher extends GwtApplication {
+	@Override
+	public GwtApplicationConfiguration getConfig () {
+		GwtApplicationConfiguration cfg = new GwtApplicationConfiguration(480, 320);
+		return cfg;
+	}
+
+	@Override
+	public ApplicationListener getApplicationListener () {
+		return new Game();
+	}
+}
